b057202e7fed404d872a01f68b63eefef5a60eb0
seraphobsidian-max <seraphobsidian@gmail.com>
2026-08-26 15:44:17 +0800

    Update hug.js
    Signed-off-by: seraphobsidian-max <seraphobsidian@gmail.com>

--- script/hug.js
+++ script/hug.js
@@ -1,21 +1,56 @@
-module.exports.config = {
-  name: "hug",
-  version: "1.0.0",
-  hasPermission: 0,
-  credits: "sinzu",
-  description: "Yakapin ang kaibigan sa GC",
-  usePrefix: true,
-  commandCategory: "Fun",
-  usages: "!hug @mention",
-  cooldowns: 3
-};
+const axios = require("axios");
 
-module.exports.run = async function({ api, event }) {
-  const { threadID, messageID, mentions } = event;
-  if (!mentions || Object.keys(mentions).length === 0) {
-    return api.sendMessage("🤗 I-tag (@mention) ang taong gusto mong yakapin!", threadID, messageID);
+module.exports = {
+  config: {
+    name: "hug",
+    aliases: ["yakap"],
+    version: "1.0.0",
+    role: 0,
+    hasPrefix: true,
+    description: "Send a hug GIF to a username",
+    usage: "/hug username",
+    credits: "sinzu",
+    cooldown: 3
+  },
+
+  run: async ({ api, event, args }) => {
+    const username = args.join(" ").trim();
+
+    if (!username) {
+      return api.sendMessage(
+        "🤗 Lagyan mo ng username!\n\nExample: /hug Juan",
+        event.threadID,
+        event.messageID
+      );
+    }
+
+    const gifs = [
+      "https://media.giphy.com/media/od5H3PmEG5EVq/giphy.gif",
+      "https://media.giphy.com/media/3ZnBrkqoaI2hq/giphy.gif",
+      "https://media.giphy.com/media/l2QDM9Jnim1YVILXa/giphy.gif"
+    ];
+
+    const gif = gifs[Math.floor(Math.random() * gifs.length)];
+
+    try {
+      const response = await axios.get(gif, {
+        responseType: "stream"
+      });
+
+      return api.sendMessage(
+        {
+          body: `🤗 ${username}, niyakap ka ni ${event.senderID}! ❤️`,
+          attachment: response.data
+        },
+        event.threadID,
+        event.messageID
+      );
+    } catch (err) {
+      return api.sendMessage(
+        `🤗 ${username}, niyakap ka! ❤️`,
+        event.threadID,
+        event.messageID
+      );
+    }
   }
-  const targetName = Object.values(mentions)[0];
-  api.sendMessage(`🤗❤️ Binigyan ng mahigpit at mainit na yakap si ${targetName}!`, threadID, messageID);
 };
-module.exports.onStart = module.exports.run;
